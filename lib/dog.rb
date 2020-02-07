7d6ff8416373a9efce6757780ea303142175769b
Oscar Ore <oscar.ore2011@gmail.com>
2020-02-07 02:47:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,13 @@
 class Dog
-  def initialize(name)
-    @name = name 
-  end 
-  
-  def name=(name)
-    @name 
-  end 
-  
-  def name 
-    @name 
-  end 
-end 
+  def initialize(breed)
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
+  end
+end
 
-Mutt = Dog.new 
+
  
